4e9c95d9f520c460869ac3ac5d6f5413cbe00ef0
John Daise <jdaise@gmail.com>
2018-03-15 13:55:03 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,22 +13,14 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
-    if katz_deli.empty?
-      katz_deli << name
-      puts "Welcome, #{name}. You are number 1 in line."
-
-    else
       katz_deli << name
       puts "Welcome, #{name}. You are number #{katz_deli.count} in line."
-
-    end
 end
 
 def now_serving(katz_deli)
   if katz_deli.empty?
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{katz_deli[0]}."
-    katz_deli.shift
+    puts "Currently serving #{katz_deli.shift}."
   end
 end
